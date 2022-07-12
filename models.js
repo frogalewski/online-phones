42050c0500e776bf54951dd3c6ca8783b6c4d726
frogalewski <frogalewski@gmail.com>
2022-07-11 22:39:28 -0300

    atualizar commit

---
+++ models.js
@@ -0,0 +1,11 @@
+let modelsJson = [
+    {id:1, name:'Iphone 13', img:'img/iphone13.png', price:[6390.00, 6880.00, 7890.00], sizes:['128gb','256gb','512gb'], description:'6GB de RAM'},
+    {id:2, name:'Sansung S21', img:'img/sansungs21.png', price:[2699.00, 3400.00, 4200.00], sizes:['128gb', '256gb', '512gb'], description:' 8GB RAM'},
+    {id:3, name:'Xiaomi MI 11 Ultra', img:'img/mi11ultra.png', price:[3690.00, 4150.00, 6200.00], sizes:['128gb', '256gb', '512gb'], description:'8GB de RAM'},
+    {id:4, name:'Moto G200', img:'img/Me109G6.png', price:[3100.00, 4550.00, 6200.00], sizes:['128gb', '256gb', '512gb'], description:'4, 6, 8GB de RAM'},
+    {id:5, name:'Galaxy Z flip', img:'img/P47D.png', price:[2080.00, 4150.00, 8200.00], sizes:['128gb', '256gb', '512gb'], description:'8, 12, 16BG de RAM'},
+    {id:6, name:'Motorola Razr', img:'img/P51D.png', price:[4999.00, 62150.00, 8200.00], sizes:['128gb', '256gb', '512gb'], description:'4, 6, 8GB de RAM'},
+    {id:7, name:'Iphone 8', img:'img/SpitMk9.png', price:[1730.00, 2150.00, 3200.00], sizes:['128gb', '256gb', '512gb'], description:'8, 12, 16BG de RAM'},
+    {id:8, name:'Oppo', img:'img/Zero.png', price:[2660.00, 5150.00, 8200.00], sizes:['128gb', '256gb', '512gb'], description:'8, 12, 16BG de RAM'}
+    
+];
